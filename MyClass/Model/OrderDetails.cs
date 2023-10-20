39598ea8d54c6b1751c961777cfb7a443a250e9a
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2023-10-20 12:08:21 +0700

    Initial commit

---
+++ MyClass/Model/OrderDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass.Model
+{
+    [Table("OrderDetails")]
+    public class OrderDetails
+    {
+        [Key]
+        public int Id { get; set; }
+        public int OrderID { get; set; }
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Qty { get; set; }
+    }
+}
