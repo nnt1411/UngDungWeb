94106bed06c56e0e76216036e1b7454dfad80da5
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2023-11-16 18:43:47 +0700

    Product Index

--- UngDungWeb/Global.asax.cs
+++ UngDungWeb/Global.asax.cs
@@ -17,5 +17,10 @@ namespace UngDungWeb
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        public void Session_Start()
+        {
+            Session["UserId"] = "1";
+        }
     }
 }
