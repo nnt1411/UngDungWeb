da8f55810b83e3cf2825ffe501b9bf14fb82b397
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2023-12-01 13:36:55 +0700

    Menu

--- MyClass/Model/Menus.cs
+++ MyClass/Model/Menus.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,18 +13,48 @@ namespace MyClass.Model
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int? TableId { get; set; }
-        public string TypeMenu { get; set; }
-        public string Position { get; set; }
-        public string Link { get; set; }
-        public int? ParentId { get; set; }
-        public int? Order { get; set; }
-        public int CreateBy { get; set; }
-        public DateTime CreateAt { get; set; }
-        public int UpdateBy { get; set; }
-        public DateTime UpdateAt { get; set; }
-        public int Status { get; set; }
-    }
 
+        [Required(ErrorMessage = "Tên menu không để trống")]
+        [Display(Name = "Tên Menu")]
+        public string Name { get; set; }
+
+        [Display(Name = "Bảng dữ liệu")]
+        public int? TableId { get; set; }
+
+        [Display(Name = "Kiểu Menu")]
+        public string TypeMenu { get; set; }
+
+        [Display(Name = "Vị trí")]
+        public string Position { get; set; }
+
+        [Display(Name = "Liên kết")]
+        public string Link { get; set; }
+
+        [Display(Name = "Cấp cha")]
+        public int? ParentID { get; set; }
+
+        [Display(Name = "Sắp xếp")]
+        public int? Order { get; set; }
+
+        [Display(Name = "Người tạo")]
+        [Required(ErrorMessage = "Người tạo không để trống")]
+        public DateTime CreateAt { get; set; }
+
+        [Display(Name = "Người tạo")]
+        [Required(ErrorMessage = "Người tạo không để trống")]
+        public int CreateBy { get; set; }
+
+        [Display(Name = "Ngày cập nhật")]
+        [Required(ErrorMessage = "Ngày cập nhật không để trống")]
+        public DateTime UpdateAt { get; set; }
+
+        [Display(Name = "Người cập nhật")]
+        [Required(ErrorMessage = "Người cập nhật không để trống")]
+        public int UpdateBy { get; set; }
+
+        [Display(Name = "Trạng thái")]
+        [Required(ErrorMessage = "Trạng thái không để trống")]
+        public int Status { get; set; }
+
+    }
 }
---
+++ UngDungWeb/Controllers/ModuleController.cs
@@ -0,0 +1,21 @@
+﻿using MyClass.DAO;
+using MyClass.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UngDungWeb.Controllers
+{
+    public class ModuleController : Controller
+    {
+        MenusDAO menusDAO = new MenusDAO();
+        ///////////////////////////////////////////////////////////////////
+        //GET: MainMenu
+        public ActionResult MainMenu()
+        {          
+            return View(menusDAO.getListByParentId(0));
+        }
+    }
+}
